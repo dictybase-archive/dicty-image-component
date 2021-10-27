ee5616549c8653e729c09ec33afdfeebdcf12026
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-27 18:16:57 -0500

    feat: create and export DictyImage

---
+++ src/components/DictyImage.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { DictyImageProps } from '../types';
+
+const DictyImage = ({
+  src,
+  nextGenSources,
+  alt,
+  id,
+  imgClassName,
+  className,
+}: DictyImageProps) => {
+  return (
+    <picture className={className}>
+      {nextGenSources?.map((s, i) => (
+        <source
+          media={`(orientation: ${
+            s.orientation ? s.orientation : 'landscape'
+          })`}
+          srcSet={s.srcSet}
+          type={s.type}
+          key={`dictyImg${alt}${i}`}
+        />
+      ))}
+      <img src={src} alt={alt} className={imgClassName} id={id} />
+    </picture>
+  );
+};
+
+export default DictyImage;
